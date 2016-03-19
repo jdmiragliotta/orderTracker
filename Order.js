7b5255e4b6a1a5b579e70261bfa9dfc43ba149e3
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-19 10:53:04 -0400

    initial commit

---
+++ Order.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var orderSchema = new Schema({
+    address : {
+      type:String,
+    },
+    notes : {
+      type:String,
+    },
+    items : [{
+      name: String,
+    }]
+});
+
+var Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
